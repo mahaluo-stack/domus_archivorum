786db31cfc8bf10b4b6fe40980f7701199d00346
max <maxhaggqvist@gmail.com>
2026-06-01 00:19:28 +0200

    depositum musculi regio

--- scriptorium_depositum/src/main/kotlin/ars_disciplina/musculorum/DepositumMusculiRegio.kt
+++ scriptorium_depositum/src/main/kotlin/ars_disciplina/musculorum/DepositumMusculiRegio.kt
@@ -2,34 +2,29 @@ package ars_disciplina.musculorum
 
 import ars_disciplina.Conexio
 import ars_disciplina.musculorum.valores.DescriptioMusculi
-import ars_disciplina.musculorum.valores.NomenMusculiPars
-import ars_disciplina.musculorum.valores.identitas.MusculiIdentitas
-import ars_disciplina.musculorum.valores.identitas.MusculiParsIdentitas
-import org.example.exemplaria.tabulae.MusculiPars
+import ars_disciplina.musculorum.valores.NomenMusculiRegio
+import ars_disciplina.musculorum.valores.identitas.MusculiRegioIdentitas
+import org.example.exemplaria.tabulae.MusculiRegio
 
 object DepositumMusculiRegio {
-    fun legeOmnes(): Set<MusculiPars> {
+    fun legeOmnes(): Set<MusculiRegio> {
         Conexio.getConnection().use { conn ->
             val sql = """
-                SELECT 
-                    musculi_identitas,
-                    nomen_musculi_pars,
+                SELECT
+                    nomen_musculi_regio,
                     descriptio
-                FROM ars_disciplina.musculorum.musculi_pars
+                FROM ars_disciplina.musculorum.musculi_regio
             """.trimIndent()
             conn.prepareStatement(sql).use { ps ->
                 ps.executeQuery().use { rs ->
-                    val collectio = mutableSetOf<MusculiPars>()
+                    val collectio = mutableSetOf<MusculiRegio>()
                     while (rs.next()) {
-                        val musculiIdentitas = MusculiIdentitas(valor = rs.getInt("musculi_identitas"))
-                        val identitas = MusculiParsIdentitas(valor = rs.getInt("musculi_pars_identitas"))
-                        val nomen = NomenMusculiPars(valor = rs.getString("nomen_musculi_pars"))
+                        val identitas = MusculiRegioIdentitas(valor = rs.getInt("musculi_regio_identitas"))
+                        val nomen = NomenMusculiRegio(valor = rs.getString("nomen_musculi_regio"))
                         val descriptio = DescriptioMusculi(valor = rs.getString("descriptio") ?: "")
-
                         collectio.add(
-                            MusculiPars(
+                            MusculiRegio(
                                 identitas,
-                                musculiIdentitas,
                                 nomen,
                                 descriptio
                             )
