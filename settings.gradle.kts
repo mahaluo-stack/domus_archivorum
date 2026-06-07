310c12929255c946fa4c4617cccbd8eb6e5daa17
max <maxhaggqvist@gmail.com>
2026-06-07 03:08:10 +0200

    restructuravit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,22 @@
 rootProject.name = "domus_archivorum"
 
-include(
-    "magnum_archivum",
-    "curia_actarii",
-    "curia_disciplinae",
-    "scriptorium_depositum",
-    "scriptorium_exemplaria",
-    "scriptorium_infrastructura",
-    "scriptorium_ministerium",
-    "scriptorium_registrum"
-)
+include(":curia_actarii")
+project(":curia_actarii").projectDir = file("collegium/officina/curia/curia_actarii")
+
+include(":curia_disciplinae")
+project(":curia_disciplinae").projectDir = file("collegium/officina/curia/curia_disciplinae")
+
+include(":scriptorium_depositum")
+project(":scriptorium_depositum").projectDir = file("collegium/officina/scriptorium/scriptorium_depositum")
+
+include(":scriptorium_exemplaria")
+project(":scriptorium_exemplaria").projectDir = file("collegium/officina/scriptorium/scriptorium_exemplaria")
+
+include(":scriptorium_infrastructura")
+project(":scriptorium_infrastructura").projectDir = file("collegium/officina/scriptorium/scriptorium_infrastructura")
+
+include(":scriptorium_ministerium")
+project(":scriptorium_ministerium").projectDir = file("collegium/officina/scriptorium/scriptorium_ministerium")
+
+include(":scriptorium_registrum")
+project(":scriptorium_registrum").projectDir = file("collegium/officina/scriptorium/scriptorium_registrum")
