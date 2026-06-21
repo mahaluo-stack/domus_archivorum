9cbbcb17969be28976a842043ead4640b21a4e49
maxhaggkvist <maxhaggqvist@gmail.com>
2026-06-21 12:06:37 +0200

    restrukturavit ministerium

--- collegium/officina/scriptorium/registrum/src/main/kotlin/ars_disciplina/registrum/nexuum/RegistrumMusculiMusculiRegio.kt
+++ collegium/officina/scriptorium/registrum/src/main/kotlin/ars_disciplina/registrum/nexuum/RegistrumMusculiMusculiRegio.kt
@@ -1,12 +1,10 @@
 package ars_disciplina.registrum.nexuum
 
-import ars_disciplina.commune.registra.RegistraMusculi
 import ars_disciplina.commune.registra.RegistraMusculiMusculiRegio
 import ars_disciplina.musculorum.valores.identitas.MusculiIdentitas
 import ars_disciplina.nexuum.tabula.MusculiMusculiRegio
-import org.example.exemplaria.tabulae.Musculi
 
-class RegistrumMusculiMusculiRegio {
+object RegistrumMusculiMusculiRegio {
     fun omnia(): Collection<MusculiMusculiRegio> = RegistraMusculiMusculiRegio.omnia()
     fun invenire(identitas: MusculiIdentitas): MusculiMusculiRegio = RegistraMusculiMusculiRegio.invenirePerClavemNecesse(identitas)
 }
--- collegium/officina/scriptorium/registrum/src/main/kotlin/ars_disciplina/registrum/nexuum/RegistrumMusculiParsMusculi.kt
+++ collegium/officina/scriptorium/registrum/src/main/kotlin/ars_disciplina/registrum/nexuum/RegistrumMusculiParsMusculi.kt
@@ -1,13 +1,10 @@
 package ars_disciplina.registrum.nexuum
 
-import ars_disciplina.commune.registra.RegistraMusculiMusculiRegio
 import ars_disciplina.commune.registra.RegistraMusculiParsMusculi
-import ars_disciplina.musculorum.valores.identitas.MusculiIdentitas
 import ars_disciplina.musculorum.valores.identitas.MusculiParsIdentitas
-import ars_disciplina.nexuum.tabula.MusculiMusculiRegio
 import ars_disciplina.nexuum.tabula.MusculiParsMusculi
 
-class RegistrumMusculiParsMusculi {
+object RegistrumMusculiParsMusculi {
     fun omnia(): Collection<MusculiParsMusculi> = RegistraMusculiParsMusculi.omnia()
     fun invenire(identitas: MusculiParsIdentitas): MusculiParsMusculi = RegistraMusculiParsMusculi.invenirePerClavemNecesse(identitas)
 }
